b93a07a84afb3cd8b09166215226c41be09a8208
Rehan Guha <rehanguha29@gmail.com>
2019-02-22 15:57:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 def prim(n):
     primes = []
-    for num in range(1, n + 1):     # current number check
+    for num in range(1, n + 1):     
         limit = int(num ** 0.5) + 1 # search limit
         for div in range(2, limit): # searching for divider
             if (num % div) == 0:    # divider found
-                break               # exit the inner loop
-        else:                       # out of the inner loop
+                break               
+        else:                       
             primes.append(num)      # no divider found hence prime
     return(primes)
 
 num = int(input('Input an even number: '))
-primes = prim(num)                  # create list of primes
+#num =100
+primes = prim(num)               
 
 for i in range(num//2 + 1):
     if num % 2 != 0:
         print(num, 'not even'); break
     if i in primes and (num - i) in primes:
-        print(i, '+', num - i)
+        print(i, num - i)
